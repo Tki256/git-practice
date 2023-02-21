32fd661fd20c94a3b2cbf319eefbeac3c68f8d9d
Tki256 <taiki900824@gmail.com>
2023-02-21 17:02:30 +0900

    First commit

---
+++ perfectNumber.py
@@ -0,0 +1,19 @@
+#変更の練習
+
+def perfectNumberList(n):
+    res = ""
+    for i in range(2,n+1):
+        if validPerfectNumber(i):
+            res += str(i) + "-"
+    return res[:-1] if res != "" else "none"
+
+
+def validPerfectNumber(n):
+    sum = 1
+    for i in range(2,n):
+        if i*i > n:
+            break
+        if n % i == 0:
+            sum += i
+            sum += n/i
+    return n == sum
